aba9bb88bb3f90ba9c7bb09a88897585588198c6
Ailana Karamchakova <karamchakova.ae@phystech.edu>
2024-11-24 14:36:53 +0300

    Class created TableOperations

--- src/main/java/test/DAO/TableOperations.java
+++ src/main/java/test/DAO/TableOperations.java
@@ -1,4 +1,20 @@
 package test.DAO;
 
-public class TabelOperations {
+import java.sql.SQLException;
+
+public interface TableOperations {
+  void CreateTable() throws SQLException;
+  void CreateForeignKeys() throws SQLException;
+
+  void GetAll() throws SQLException;
+  void Update(int id, String row, String column, String information) throws SQLException;
+  void Delete(int id, String row) throws SQLException;
+
+  void GetByIdUser(int id) throws SQLException;
+  void CreateUser()  throws SQLException;
+
+  void GetByIdDevices(int id) throws SQLException;
+  void CreateDevices()  throws SQLException;
+
+
 }
