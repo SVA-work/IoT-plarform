39cb5ee5bd2df7808115f73349f9d6ddf8ad9f02
Ishak <vesilgame2006@gmail.com>
2025-01-17 17:46:27 +0300

    Добавление телеграмма при регистрации

--- src/main/java/service/TelemetryService.java
+++ src/main/java/service/TelemetryService.java
@@ -8,21 +8,14 @@ import dto.DbConnectionDto;
 import dto.devices.MicroclimateSensor;
 import dto.entity.DeviceDto;
 import dto.entity.RuleDto;
-import dto.entity.TelegramTokenDto;
 import tables.DevicesRepository;
-import tables.TelegramTokenRepository;
-import telegrambot.bot.IoTServiceBot;
 
 public class TelemetryService {
 
   private final DevicesRepository devicesRepository;
-  //private final IoTServiceBot iotServiceBot;
-  private final TelegramTokenRepository telegramTokenRepository;
 
   public TelemetryService(DbConnectionDto dbConnectionDto) {
     devicesRepository = new DevicesRepository(dbConnectionDto);
-    //iotServiceBot = new IoTServiceBot("7614249328:AAF6E3EFO1yLqxlbQhBCR4P977EA8VWxuWY");
-    telegramTokenRepository = new TelegramTokenRepository(dbConnectionDto);
   }
 
   public String decodeBase64(String base64Data) {
@@ -45,16 +38,6 @@ public class TelemetryService {
       }
     }
 
-    TelegramTokenDto telegramToken = new TelegramTokenDto();
-    device = devicesRepository.getById(device);
-    telegramToken.setUserId(device.getUserId());
-    List<TelegramTokenDto> telegramTokens = telegramTokenRepository.getAll();
-    for (TelegramTokenDto telegramTokennDto : telegramTokens) {
-      if (telegramToken.getUserId().equals(telegramTokennDto.getUserId())) {
-        telegramToken.setTelegramToken(telegramTokennDto.getTelegramToken());
-      }
-    }
-
     List<RuleDto> allRulesOfDevice = devicesRepository.rulesOfDevice(device);
     for (RuleDto ruleDto : allRulesOfDevice) {
       String rule = ruleDto.getRule();
@@ -64,10 +47,10 @@ public class TelemetryService {
         float lowTemperature = Float.parseFloat(parts[1]);
         float hightTemperature = Float.parseFloat(parts[2]);
         if (deviceTemperature < lowTemperature) {
-          //iotServiceBot.sendLowerTempNotification(telegramToken.getTelegramToken(), device.getToken(), parts[1]);
+          // говорим пользователю, что температура слишком низкая
         }
         if (deviceTemperature > hightTemperature) {
-          //iotServiceBot.sendLowerTempNotification(telegramToken.getTelegramToken(), device.getToken(), parts[2]);
+          // говорим пользователю, что температура слишком высокая
         }
       }
     }
