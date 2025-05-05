a1d6e56515fadce0c6a6ba733be180884e15d4dd
Andreyka71 <andrey.yakovlev.al@gmail.com>
2025-05-05 19:29:45 +0300

    рефакторинг

--- src/main/java/application/controller/deviceapi/TelemetryHttpController.java
+++ src/main/java/application/controller/deviceapi/TelemetryHttpController.java
@@ -6,11 +6,15 @@ import application.netty.library.annotation.Post;
 import application.netty.library.annotation.RequestBody;
 import application.netty.library.json.JsonParserDefault;
 import application.service.TelemetryService;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.FullHttpResponse;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -26,8 +30,20 @@ public class TelemetryHttpController extends AbstractHttpMappingHandler {
     }
 
     @Post("/telemetry")
-    public ResponseEntity<Void> report(@RequestBody MicroclimateSensor message) throws JsonProcessingException {
+    public FullHttpResponse report(@RequestBody MicroclimateSensor message) throws JsonProcessingException {
         log.info("Полученно сообщение от устройства по http");
-        return telemetryService.reportProcessingAndSend(message);
+        try {
+            telemetryService.reportProcessingAndSend(message);
+            return new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1, 
+                HttpResponseStatus.OK
+            );
+        } catch (Exception e) {
+            return new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1, 
+                HttpResponseStatus.INTERNAL_SERVER_ERROR,
+                Unpooled.wrappedBuffer(e.getMessage().getBytes())
+            );
+        }
     }
 }
